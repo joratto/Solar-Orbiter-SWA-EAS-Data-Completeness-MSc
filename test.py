b4903fed4b283630823851f7e8fb5fefa5746473
Joaquim Dickson <47600979+joratto@users.noreply.github.com>
2024-01-25 17:28:37 +0000

    test.py imports working

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+import sys
+print(sys.executable)
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+os.environ["CDF_LIB"] = "C:\\Program Files\\CDF_Distribution"
+from spacepy import pycdf
 
 print('Hello Joaquim')
