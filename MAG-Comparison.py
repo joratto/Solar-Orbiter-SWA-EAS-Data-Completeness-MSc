982af9e7968ae321b457ad74d3493f7eb7216a9c
Joaquim Dickson <47600979+joratto@users.noreply.github.com>
2024-01-30 18:41:17 +0000

    fixed time offset and added angle plot

--- MAG-Comparison.py
+++ MAG-Comparison.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import seaborn as sns
 import os
 os.environ["CDF_LIB"] = "C:\\Program Files\\CDF_Distribution"
@@ -42,20 +43,34 @@ print('\n')
 
 time_mag = time_mag[time_mag_index_t0:time_mag_index_tf]
 
-B_mag = cdf_mag['B_SRF'][time_mag_index_t0:time_mag_index_tf] # reported MAG B, cropped to match EAS timeframe
+B_mag = cdf_mag['B_SRF'][500000:]
+B_mag = B_mag[time_mag_index_t0:time_mag_index_tf] # reported MAG B, cropped to match EAS timeframe
 B_eas = cdf_eas['SWA_EAS_MagDataUsed'] # onboard EAS B
 
-# normalise B_mag: 
-B_mag_norm = np.array([])
+B_angle = np.ndarray((len(B_mag)))
 for i in range(len(B_mag)):
-    vector = B_mag[i]
-    B_mag[i] = vector/np.linalg.norm(vector)
+    # normalise B_mag:
+    vector_mag = B_mag[i]
+    B_mag[i] = vector_mag/np.linalg.norm(vector_mag)
+
+    #calculate angles between MAG vectors and EAS vectors:
+    vector_eas = B_eas[i]
+    B_angle[i] = np.arccos(np.dot(vector_mag,vector_eas))*180/np.pi
+
+vector_mag = B_mag[-10]
+vector_eas = B_eas[-10]
+print(vector_mag)
+print(vector_eas)
+print(np.arccos(np.dot(vector_mag,vector_eas))*180/np.pi)
 
 Bx_mag, By_mag, Bz_mag = np.array(B_mag).T
 Bx_eas, By_eas, Bz_eas = np.array(B_eas).T
 
+Nplots = 4
 sns.set_theme(style='ticks')
-fig, axs = plt.subplots(3)
+fig, axs = plt.subplots(Nplots)
+
+axs[0].set_title('{}    &    {}'.format(cdf_filename_eas, cdf_filename_mag))
 
 axs[0].plot(time_mag,Bx_mag)
 axs[0].plot(time_eas,Bx_eas)
@@ -63,7 +78,6 @@ axs[0].set_ylabel('Bx (normalised)')
 axs[0].legend(["Bx MAG", "Bx EAS"])
 axs[0].set_ylim(-1.2,1.2)
 axs[0].grid()
-axs[0].set_title('{}    &    {}'.format(cdf_filename_eas, cdf_filename_mag))
 
 axs[1].plot(time_mag,By_mag)
 axs[1].plot(time_eas,By_eas)
@@ -78,7 +92,15 @@ axs[2].set_ylabel('Bz (normalised)')
 axs[2].legend(["Bz MAG", "Bz EAS"])
 axs[2].set_ylim(-1.2,1.2)
 axs[2].grid()
-axs[2].set_xlabel('Date & Time (dd hh:mm)')
+
+axs[3].plot(time_mag,B_angle, color='green')
+axs[3].set_ylabel('angle between B vectors (°)')
+tick_spacing = 5 # degrees
+axs[3].yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+#axs[3].yaxis.set_major_formatter('{x}°')
+axs[3].grid()
+
+axs[Nplots-1].set_xlabel('Date & Time (dd hh:mm)')
 
 # quick calculation of the angle difference between B vectors
 print('{} degrees'.format(180*(np.arctan(Bz_mag[0]/Bx_mag[0])/90/np.pi - np.arctan(Bz_eas[0]/Bx_eas[0])/np.pi)))
