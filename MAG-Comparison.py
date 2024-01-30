756716c5002d4dbde3ea648a29ee44188d7c01ed
Joaquim Dickson <47600979+joratto@users.noreply.github.com>
2024-01-30 22:54:21 +0000

    added "efficient" time_mag index finder

--- MAG-Comparison.py
+++ MAG-Comparison.py
@@ -9,33 +9,29 @@ import datetime
 
 cdf_filename_eas = 'solo_L1_swa-eas-padc_20230831T172734-20230831T173233_V01.cdf'
 cdf_eas = pycdf.CDF('data\\' + cdf_filename_eas)
-#print(cdf_eas)
-#print(cdf_eas['SWA_EAS_MagDataUsed'][:])
 time_eas = np.array(cdf_eas['EPOCH'])
 t0 = time_eas[0] # start of timeframe
 tf = time_eas[-1] # end of timeframe
 print('EAS time series from {} to {}'.format(t0, tf))
-#for vector in cdf_eas['SWA_EAS_MagDataUsed']:
-#    print(np.sqrt(vector[0]**2+vector[1]**2+vector[2]**2))
 
 cdf_filename_mag = 'solo_L2_mag-srf-normal_20230831_V01.cdf'
 cdf_mag = pycdf.CDF('data\\' + cdf_filename_mag)
-#print(cdf_mag)
-time_mag = np.array(cdf_mag['EPOCH'][500000:])
-
-length = len(time_mag)
+time_mag = cdf_mag['EPOCH']#[500000:])
 
 # The following code block finds where to crop time_mag and
-#B_mag to match time_eas and B_eas (can be more efficient).
+# B_mag to match time_eas and B_eas (can be more efficient).
 print('\n')
+length = len(time_mag)
 time_mag_index_t0 = 0
-while time_mag[time_mag_index_t0] < t0: #datetime.datetime(1970,1,1):#t0:
+for i in range(1,5):
+    time_mag_index_t0 += int(length/2**i)*(time_mag[time_mag_index_t0+int(length/2**i)] < t0) # add smaller and smaller slices to converge on the right time_mag index for t0
+while time_mag[time_mag_index_t0] < t0:
     time_mag_index_t0 += 1
     print('\rt0 MAG index = {}/{}'.format(time_mag_index_t0,length), end='')
     continue
 print('\n')
 time_mag_index_tf = time_mag_index_t0
-while time_mag[time_mag_index_tf] < tf: #datetime.datetime(1970,1,1):#
+while time_mag[time_mag_index_tf] < tf:
     time_mag_index_tf += 1
     print('\rtf MAG index = {}/{}'.format(time_mag_index_tf,length), end='')
     continue
@@ -43,7 +39,7 @@ print('\n')
 
 time_mag = time_mag[time_mag_index_t0:time_mag_index_tf]
 
-B_mag = cdf_mag['B_SRF'][500000:]
+B_mag = cdf_mag['B_SRF']#[500000:]
 B_mag = B_mag[time_mag_index_t0:time_mag_index_tf] # reported MAG B, cropped to match EAS timeframe
 B_eas = cdf_eas['SWA_EAS_MagDataUsed'] # onboard EAS B
 
@@ -57,11 +53,13 @@ for i in range(len(B_mag)):
     vector_eas = B_eas[i]
     B_angle[i] = np.arccos(np.dot(vector_mag,vector_eas))*180/np.pi
 
+'''
 vector_mag = B_mag[-10]
 vector_eas = B_eas[-10]
 print(vector_mag)
 print(vector_eas)
 print(np.arccos(np.dot(vector_mag,vector_eas))*180/np.pi)
+'''
 
 Bx_mag, By_mag, Bz_mag = np.array(B_mag).T
 Bx_eas, By_eas, Bz_eas = np.array(B_eas).T
@@ -103,8 +101,7 @@ axs[3].grid()
 axs[Nplots-1].set_xlabel('Date & Time (dd hh:mm)')
 
 # quick calculation of the angle difference between B vectors
-print('{} degrees'.format(180*(np.arctan(Bz_mag[0]/Bx_mag[0])/90/np.pi - np.arctan(Bz_eas[0]/Bx_eas[0])/np.pi)))
-print('{} degrees'.format(180*(np.arctan(By_mag[0]/Bx_mag[0])/90/np.pi - np.arctan(By_eas[0]/Bx_eas[0])/90/np.pi)))
+# print('{} degrees'.format(180*(np.arctan(Bz_mag[0]/Bx_mag[0])/90/np.pi - np.arctan(Bz_eas[0]/Bx_eas[0])/np.pi)))
+# print('{} degrees'.format(180*(np.arctan(By_mag[0]/Bx_mag[0])/90/np.pi - np.arctan(By_eas[0]/Bx_eas[0])/90/np.pi)))
 
 plt.show()
-
