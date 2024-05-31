ffab8d99a5c2585a77f7612d9b6ca9a1636e643d
Joaquim Dickson <47600979+joratto@users.noreply.github.com>
2024-05-31 03:11:49 +0100

    simplified variables
    removed some confusing variable names inherited from mag_comparison. Also added an example with numpy.

--- CDF_Viewer.py
+++ CDF_Viewer.py
@@ -1,8 +1,13 @@
+import numpy as np
 import os
 os.environ["CDF_LIB"] = "C:\\Program Files\\CDF_Distribution"
 from spacepy import pycdf
 
 cdf_filename = 'solo_L1_swa-eas-padc_20230530T172732-20230530T173231_V01.cdf'
-cdf_eas = pycdf.CDF('data\\' + cdf_filename)
+cdf = pycdf.CDF('data\\' + cdf_filename)
 
-print(cdf_eas)
+print(cdf)
+
+example = cdf['EPOCH']
+
+print(np.array(example))
