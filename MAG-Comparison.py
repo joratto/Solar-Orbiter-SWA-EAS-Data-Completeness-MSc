c2f312e6f353abb5ecfeaef1817dab0ea741c06d
Joaquim Dickson <47600979+joratto@users.noreply.github.com>
2024-06-04 15:19:07 +0100

    Added SRF to EASX converter
    Added function to convert SRF to EASX (X=1 or 2).
    Added a way to quickly choose which coordinates to plot in.
    Added a way to plot the EAS sensor head used.
    
    Some numbers are subject to change based on Chris's confirmation.

--- MAG-Comparison.py
+++ MAG-Comparison.py
@@ -8,7 +8,7 @@ os.environ["CDF_LIB"] = "C:\\Program Files\\CDF_Distribution"
 from spacepy import pycdf
 import datetime
 
-cdf_filename_eas = 'solo_L1_swa-eas-padc_20230530T172732-20230530T173231_V01.cdf'#solo_L1_swa-eas-padc_20230831T172734-20230831T173233_V01.cdf'
+cdf_filename_eas = 'solo_L1_swa-eas-padc_20231105T172733-20231105T174232_V01.cdf'
 cdf_eas = pycdf.CDF('data\\' + cdf_filename_eas)
 #print(cdf_eas)
 time_eas = np.array(cdf_eas['EPOCH'])
@@ -17,7 +17,7 @@ t0 = time_eas[0] # start of timeframe
 tf = time_eas[-1] # end of timeframe
 print('EAS time series from {} to {}'.format(t0, tf))
 
-cdf_filename_mag = 'solo_L2_mag-srf-normal_20230530_V01.cdf'#'solo_L2_mag-srf-normal_20230831_V01.cdf'
+cdf_filename_mag = 'solo_L2_mag-srf-normal_20231105_V01.cdf'#'solo_L2_mag-srf-normal_20230831_V01.cdf'
 cdf_mag = pycdf.CDF('data\\' + cdf_filename_mag)
 #print(cdf_mag)
 time_mag = cdf_mag['EPOCH']#[500000:])
@@ -97,16 +97,24 @@ def cropTimeToOverlap(seriesA,timeA,seriesB,timeB,searchdivisions=5):
 
     return seriesA, timeA, seriesB, timeB
 
+r2o2 = np.sqrt(2)/2 # root 2 over 2
+SRFtoEAS1 = np.array([[0,0,-1],[-r2o2,r2o2,0],[r2o2,r2o2,0]])
+SRFtoEAS2 = np.array([[0,0,1],[-r2o2,-r2o2,0],[r2o2,-r2o2,0]])
+SRFtoEASX = (SRFtoEAS1,SRFtoEAS2) # the transform matrices for SRF to the respective EAS head coordinates. First is EAS1, second is EAS2.
 
-B_eas = cdf_eas['SWA_EAS_MagDataUsed'] # onboard EAS B
-B_mag = cdf_mag['B_SRF'] # reported MAG B
+B_eas = cdf_eas['SWA_EAS_MagDataUsed'] # onboard EAS B (SRF)
+B_mag = cdf_mag['B_SRF'] # reported MAG B (SRF)
 
 B_mag, time_mag, B_eas, time_eas = cropTimeToOverlap(B_mag, time_mag, B_eas, time_eas, searchdivisions=5)
 
-B_eas_magnitude = np.ndarray((len(B_eas)))
-B_mag_magnitude = np.ndarray((len(B_mag)))
+B_eas_magnitude = np.ndarray((len(B_eas))) # (SRF)
+B_mag_magnitude = np.ndarray((len(B_mag))) # (SRF)
 
 B_angle = np.ndarray((len(B_mag)))
+
+eas_used = cdf_eas['SWA_EAS_EasUsed']
+B_eas_EASX = np.ndarray((len(B_eas),3)) # (EASX)
+B_mag_EASX = np.ndarray((len(B_mag),3)) # (EASX)
 
 for i in range(len(B_mag)):
     # calculate vectors and magnitudes:
@@ -119,13 +127,23 @@ for i in range(len(B_mag)):
     B_eas_magnitude[i] = vector_eas_magnitude
 
     # normalise B_mag:
-    B_mag[i] = vector_mag/vector_mag_magnitude
+    vector_mag = vector_mag/vector_mag_magnitude #!!!
+    B_mag[i] = vector_mag
 
     # calculate angles between MAG vectors and EAS vectors:
     B_angle[i] = np.arccos(np.dot(vector_mag,vector_eas))*180/np.pi
 
+    # transform from SRF to respective EAS head coordinates:
+    vector_mag_magnitude_EASX = SRFtoEASX[eas_used[i]].dot(vector_mag)
+    vector_eas_magnitude_EASX = SRFtoEASX[eas_used[i]].dot(vector_eas)
+    B_mag_EASX[i] = np.array(vector_mag_magnitude_EASX)
+    B_eas_EASX[i] = np.array(vector_eas_magnitude_EASX)
+
 Bx_mag, By_mag, Bz_mag = np.array(B_mag).T
 Bx_eas, By_eas, Bz_eas = np.array(B_eas).T
+
+Bx_mag_EASX, By_mag_EASX, Bz_mag_EASX = np.array(B_mag_EASX).T
+Bx_eas_EASX, By_eas_EASX, Bz_eas_EASX = np.array(B_eas_EASX).T
 
 def delayFinder(f,g,sr): 
     # f and g are the functions to cross-correlate
@@ -157,43 +175,58 @@ for i in range(len(corrBx)):
     corrBy[i] = np.linalg.norm(corrBy[i])
     corrBz[i] = np.linalg.norm(corrBz[i])
 
+coordinates = 'SRF' # 'SRF','EASX'
+if coordinates == 'SRF':
+    Bx_mag, By_mag, Bz_mag = Bx_mag, By_mag, Bz_mag
+    Bx_eas, By_eas, Bz_eas = Bx_eas, By_eas, Bz_eas
+elif coordinates == 'EASX':
+    Bx_mag, By_mag, Bz_mag = Bx_mag_EASX, By_mag_EASX, Bz_mag_EASX
+    Bx_eas, By_eas, Bz_eas = Bx_eas_EASX, By_eas_EASX, Bz_eas_EASX
+
 Nplots = 4
 sns.set_theme(style='ticks')
-fig, axs = plt.subplots(Nplots)
+fig1, axs = plt.subplots(Nplots)
 
 axs[0].set_title('{}    &    {}'.format(cdf_filename_eas, cdf_filename_mag))
 
 axs[0].plot(time_mag,Bx_mag)
 axs[0].plot(time_eas,Bx_eas)
-axs[0].set_ylabel(r'$B_{x}$'+'\n(normalised)')
+axs[0].set_ylabel(r'$B_{x}$'+'\n(unit {})'.format(coordinates))
 axs[0].legend([r"$B_{MAG,x}$", r"$B_{EAS,x}$"])
 axs[0].set_ylim(-1.2,1.2)
 axs[0].grid()
 
 axs[1].plot(time_mag,By_mag)
 axs[1].plot(time_eas,By_eas)
-axs[1].set_ylabel(r'$B_{y}$'+'\n(normalised)')
+axs[1].set_ylabel(r'$B_{y}$'+'\n(unit {})'.format(coordinates))
 axs[1].legend([r"$B_{MAG,y}$", r"$B_{EAS,y}$"])
 axs[1].set_ylim(-1.2,1.2)
 axs[1].grid()
 
 axs[2].plot(time_mag,Bz_mag)
 axs[2].plot(time_eas,Bz_eas)
-axs[2].set_ylabel(r'$B_{z}$'+'\n(normalised)')
+axs[2].set_ylabel(r'$B_{z}$'+'\n(unit {})'.format(coordinates))
 axs[2].legend([r"$B_{MAG,z}$", r"$B_{EAS,z}$"])
 axs[2].set_ylim(-1.2,1.2)
 axs[2].grid()
 
+axs[3].plot(time_eas,eas_used,color='green')
+axs[3].set_ylabel('Sensor Used')
+axs[3].set_ylim(-0.2,1.2)
+axs[3].set_yticks([0,1],['EAS1','EAS2'])
+axs[3].grid()
+
+'''
 axs[3].plot(time_mag,B_angle, color='green')
 #axs[3].plot(time_eas[:-1],B_angle, color='red') # in case you want to compare the two same series! (one is  currently one point shorter, for some reason)
 axs[3].set_ylabel('angle between\nB vectors (°)')
-tick_spacing = 2 # degrees
-axs[3].yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+#tick_spacing = 2 # degrees
+#axs[3].yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
 axs[3].legend([r'$B_{MAG}-B_{EAS}$ Angle'])
 axs[3].grid()
 
 axs[Nplots-1].set_xlabel('Date & Time\n(dd hh:mm)')#axs[Nplots-1].set_xlabel('Date & Time (dd hh:mm)')
-
+'''
 '''
 axs[0].plot(time_mag,Bx_mag)
 axs[0].plot(time_eas,Bx_eas)
