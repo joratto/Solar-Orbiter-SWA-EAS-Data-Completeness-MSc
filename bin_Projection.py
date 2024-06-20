99edefc4b356cdbadf4825f540dc9650473f48a4
Joaquim Dickson <47600979+joratto@users.noreply.github.com>
2024-06-20 12:34:02 +0100

    more bin_Projection variables
    replaced some literal values in bin_Projection with variables

--- bin_Projection.py
+++ bin_Projection.py
@@ -18,10 +18,13 @@ from useful_Matrices import *
 '''bin projection'''
 Nplots = 1 # number of plots to show
 #sns.set_theme(style='darkgrid')
-fig, ax = plt.subplots(Nplots)
+fig, ax = plt.subplots(Nplots,figsize=(16,18))
+#sns.set_theme(style='dark')
+plt.style.use("dark_background")
 
-point_density = 10
+point_density = 20
 
+# # project all bins for both heads and save them as .csv in bin_projections
 # for headToProject in range(2):
 #         bin_bounds = fx.getEASXBinProjections(headToProject,bin_dictionary,EASXtoSRF,point_density=point_density)
 #         for i in range(len(bin_bounds)):
@@ -31,22 +34,18 @@ point_density = 10
 #                         os.makedirs(foldername)
 #                 for j in range(len(elev)):
 #                         azim = elev[j]
-#                         ax.scatter(azim[2],azim[1],color='blue',marker='.',s=0.1)
+#                         # ax.scatter(azim[2],azim[1],color='blue',marker='.',s=0.1)
 #                         filename = foldername + '\EAS{}_el{}_az{}.csv'.format(headToProject+1, i, j)
 #                         np.savetxt(filename, np.array(azim), delimiter=",")
 
+
+# read and plot all projected bins in blue, selected elevation bins in red, and selected azimuth bins in green
 headToRead = 0
-print('\nreading bins:')
-for i in range(16):
-        for j in range(32):
-                filename = 'bin_projections\EAS{}\el{}\EAS{}_el{}_az{}.csv'.format(headToRead+1, i, headToRead+1, i, j)
-                pixelArray = np.genfromtxt(filename, delimiter=",")
-                pointCount = len(pixelArray)
-                el = pixelArray[1]
-                az = pixelArray[2]
-                ax.scatter(az,el,color='blue',marker='.',s=0.1)
-                print('\relevation = {}, azimuth = {}'.format(i, j), end='')
-print('')
+el_selected = [0,2]
+az_selected = []#[2,9]
+pix_selected = [[0,0],[4,4],[8,8],[12,12]]
+fx.plotBinProjections(ax,headToRead,el_selected,az_selected,pix_selected,pix_color='g',s=2)
+
 
 headToProject = 0
 
@@ -57,6 +56,10 @@ headToProject = 0
 #                 ax.scatter(azim[2],azim[1],color='red',marker='.',s=0.1)
 
 time = datetime.datetime(2023,11,5,hour=17,minute=29,second=38)
+
+eas_color = 'green'
+mag_color = 'purple'
+s=2
 
 vectorEAS = np.array([1, 39.611968353947965, 294.52237316250864])
 vectorEAS[2] = (vectorEAS[2]) - 360*(vectorEAS[2] > 180)
@@ -69,22 +72,22 @@ vectorMAG, antivectorMAG, contourArrayMAG = fx.getEASXVectorProjections(headToPr
 contourRadiusEAS, contourElevEAS, contourAzimEAS = contourArrayEAS
 contourRadiusMAG, contourElevMAG, contourAzimMAG = contourArrayMAG
 
-ax.scatter(contourAzimEAS,contourElevEAS,color='purple',marker='.',s=0.1)
-ax.scatter(contourAzimMAG,contourElevMAG,color='green',marker='.',s=0.1)
+ax.scatter(contourAzimEAS,contourElevEAS,color=eas_color,marker='.',s=s)
+ax.scatter(contourAzimMAG,contourElevMAG,color=mag_color,marker='.',s=s)
 
 # shaped markers
 markersize = 15
-ax.plot(vectorEAS[2],vectorEAS[1],markerfacecolor='none',markeredgecolor='purple',marker='D',markeredgewidth=2,markersize=markersize)
-ax.plot(antivectorEAS[2],antivectorEAS[1],markerfacecolor='none',markeredgecolor='purple',marker='s',markeredgewidth=2,markersize=markersize)
-ax.plot(vectorMAG[2],vectorMAG[1],markerfacecolor='none',markeredgecolor='green',marker='D',markeredgewidth=2,markersize=markersize)
-ax.plot(antivectorMAG[2],antivectorMAG[1],markerfacecolor='none',markeredgecolor='green',marker='s',markeredgewidth=2,markersize=markersize)
+ax.plot(vectorEAS[2],vectorEAS[1],markerfacecolor='none',markeredgecolor=eas_color,marker='D',markeredgewidth=2,markersize=markersize)
+ax.plot(antivectorEAS[2],antivectorEAS[1],markerfacecolor='none',markeredgecolor=eas_color,marker='s',markeredgewidth=2,markersize=markersize)
+ax.plot(vectorMAG[2],vectorMAG[1],markerfacecolor='none',markeredgecolor=mag_color,marker='D',markeredgewidth=2,markersize=markersize)
+ax.plot(antivectorMAG[2],antivectorMAG[1],markerfacecolor='none',markeredgecolor=mag_color,marker='s',markeredgewidth=2,markersize=markersize)
 
 # point markers
 pointsize = 2
-ax.plot(vectorEAS[2],vectorEAS[1],color='purple',marker='.',markeredgewidth=2,markersize=pointsize)
-ax.plot(antivectorEAS[2],antivectorEAS[1],color='purple',marker='.',markeredgewidth=2,markersize=pointsize)
-ax.plot(vectorMAG[2],vectorMAG[1],color='green',marker='.',markeredgewidth=2,markersize=pointsize)
-ax.plot(antivectorMAG[2],antivectorMAG[1],color='green',marker='.',markeredgewidth=2,markersize=pointsize)
+ax.plot(vectorEAS[2],vectorEAS[1],color=eas_color,marker='.',markeredgewidth=2,markersize=pointsize)
+ax.plot(antivectorEAS[2],antivectorEAS[1],color=eas_color,marker='.',markeredgewidth=2,markersize=pointsize)
+ax.plot(vectorMAG[2],vectorMAG[1],color=mag_color,marker='.',markeredgewidth=2,markersize=pointsize)
+ax.plot(antivectorMAG[2],antivectorMAG[1],color=mag_color,marker='.',markeredgewidth=2,markersize=pointsize)
 
 
 ax.set_title('EAS{}, {}'.format(headToProject+1, time))
